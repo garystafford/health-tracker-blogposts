f0b3a8ef05033e4a178e3476b7b55f389a679c15
Gary A. Stafford <garystafford@rochester.rr.com>
2013-11-08 00:02:58 -0500

    Fixed new unti tests. Added data setup script to console apps unti test.

--- CodeFirstPost.UnitTests/HealthTrackerUnitTests.cs
+++ CodeFirstPost.UnitTests/HealthTrackerUnitTests.cs
@@ -1,34 +1,53 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HealthTracker.ConsoleApp;
+using HealthTracker.DataAccess;
 
 namespace CodeFirstPost.UnitTests
 {
     [TestClass]
     public class HealthTrackerUnitTests
     {
+        /// <summary>
+        /// Create sample data in HealthTracker database
+        /// </summary>
+        [TestInitialize]
+        public void CreateSampleData()
+        {
+            const string filePath = "HealthTracker_Sample_Data.sql";
+            var streamReader = new StreamReader(filePath);
+            var sqlScript = streamReader.ReadToEnd();
+            streamReader.Close();
+            using (var dbContext = new HealthTrackerContext())
+            {
+                var parameters = new object[] { };
+                dbContext.Database.ExecuteSqlCommand(sqlScript, parameters);
+            }
+        }
+
         [TestMethod]
-        public void FindPersonTest()
+        public void FindPersonTestReturnsValueof0()
         {
             {
-                const string personName = "Test Person";
+                const string personName = "Test Person 1";
                 var results = Examples.FindPerson(personName);
                 Assert.AreEqual(0, results);
             }
         }
 
         [TestMethod]
-        public void CreatePersonTest()
+        public void CreatePersonTestReturnsValueOf1()
         {
             {
-                const string personName = "Test Person";
+                const string personName = "Test Person 2";
                 var results = Examples.CreatePerson(personName);
                 Assert.AreEqual(1, results);
             }
         }
 
         [TestMethod]
-        public void CreateMealsTest()
+        public void CreateMealsTestReturnsValueOf2()
         {
             {
                 var results = Examples.CreateMeals(1);
---
+++ CodeFirstPost.UnitTests/HealthTracker_Sample_Data.sql
@@ -0,0 +1,19 @@
+﻿/*
+	Remove previous data into HealthTrackerCodeFirst database
+*/
+
+-- Delete all existing records
+DELETE FROM [dbo].[Activities]
+DELETE FROM [dbo].[Hydrations]
+DELETE FROM [dbo].[Meals]
+DELETE FROM [dbo].[People]
+--DELETE FROM [dbo].[ActivityTypes]
+--DELETE FROM [dbo].[MealTypes]
+
+-- Insert People
+SET IDENTITY_INSERT [dbo].[People] ON
+INSERT INTO [dbo].[People] ([PersonId], [Name]) VALUES (1, N'John Doe')
+INSERT INTO [dbo].[People] ([PersonId], [Name]) VALUES (2, N'Maria Garcia')
+INSERT INTO [dbo].[People] ([PersonId], [Name]) VALUES (3, N'Rahul Gupta')
+INSERT INTO [dbo].[People] ([PersonId], [Name]) VALUES (4, N'Shreya Chopra')
+SET IDENTITY_INSERT [dbo].[People] OFF
